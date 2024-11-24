364c5e752394d4c9c95580d4b87d669476880965
emp-temp <emptemp0000@gmail.com>
2024-11-24 14:54:38 +0900

    update nvim config

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -81,7 +81,7 @@ return {
             require("lualine").setup({
               options = {
                 icons_enabled = true,
-                theme = 'monokai',
+                theme = 'auto',
                 section_separators = { left = '', right = '' },
                 component_separators = { left = '', right = '' },
                 disabled_filetypes = {}
